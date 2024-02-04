f8f820e19b8d9fba1e9c429c37516132f283ef71
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-04 23:01:44 +0500

    mcwnov

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// var son = 52814;
+// var sonStr = son.toFixed();
+// var yigindi = 0;
+
+// for (var i = 0; i < sonStr.length; i++) {
+//   yigindi += parseInt(sonStr[i]);
+// }
+
+// console.log(yigindi);
+// ----------------------------------------------
 var son = 52814;
 var sonStr = son.toFixed();
 var yigindi = 0;
+var i = 0;
 
-for (var i = 0; i < sonStr.length; i++) {
+while (i < sonStr.length) {
   yigindi += parseInt(sonStr[i]);
+  i++;
 }
-
 console.log(yigindi);
